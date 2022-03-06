32b87ee9c4de5420dad5a2b2f4070da9145e30bf
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2022-03-06 17:40:17 +0530

    modified project link

--- ReactTS/src/models/Project.tsx
+++ ReactTS/src/models/Project.tsx
@@ -1,6 +1,12 @@
+interface ProjectLink {
+    name: string,
+    link: string,
+}
+
+
 interface Project {
     name: string,
-    projectLink: string,
+    projectLinks: Array<ProjectLink>,
     githubLink: string,
     description: Array<string>,
 }
